a45e48ffbd1d280fc30bc8deb0216cff609520e7
tleidenfrost <P@ckers18q2>
2018-03-16 15:06:11 -0500

    new files

---
+++ commaCode.py
@@ -0,0 +1,9 @@
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+# This is an example form
+# def commaCode(someList):
+#     if len(someList) == 1:
+#         return someList[0]
+#     return '{}, and {}'.format(', '.join(someList[:-1]), someList[-1])
+
+print(commaCode(spam))
