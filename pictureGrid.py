bc5be1417f7e346258aad605916fd9207d74aa5e
taintedllama <35349285+taintedllama@users.noreply.github.com>
2018-03-21 19:42:25 -0500

    new files

--- pictureGrid.py
+++ pictureGrid.py
@@ -8,8 +8,20 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-xlength = len(grid)
-ylength = len(grid[0])
+# reddit example
+# for x in range(len(grid[0])):
+#     for y in range(len(grid)):
+#         if y == len(grid)-1:
+#             print(grid[y][x])
+#         else:
+#             print(grid[y][x],end="")
 
-lengths = xlength + ' ' + ylength)
-print(lengths)
+
+# example given by author
+for j in range(len(grid[0])):           # sets j to the current row in the loop
+    for i in range(len(grid)):          # sets i to the character within a row
+        print(grid[i][j],end='')        #
+    print('')
+
+print(len(grid))
+
